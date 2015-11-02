c7febbcae4916148501ec848fb8f35ab8361072a
Matthew Spence <msaspence@gmail.com>
2015-11-02 17:00:45 +0000

    Implemented a deploy action to send to s3

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 var argv = require('yargs').argv,
-    gulp = require('gulp'),
-    del = require('del'),
-    reactify = require("reactify"),
     browserify = require('browserify'),
     browserSync = require('browser-sync').create(),
     buffer = require('vinyl-buffer'),
-    historyApiFallback = require('connect-history-api-fallback'),
-    less = require('gulp-less'),
-    cssmin = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
+    cssmin = require('gulp-minify-css'),
+    del = require('del'),
+    fs = require('fs'),
+    gulp = require('gulp'),
+    historyApiFallback = require('connect-history-api-fallback'),
     jshint = require('gulp-jshint'),
+    less = require('gulp-less'),
     mocha = require('gulp-mocha')
     package = require('./package.json'),
     path = require('path'),
-    source = require('vinyl-source-stream');
+    s3 = require("gulp-s3"),
+    source = require('vinyl-source-stream')
+    reactify = require("reactify"),
+    uglify = require('gulp-uglify');
 
 require('./test/setup');
 require('./test/compiler');
@@ -26,13 +28,26 @@ gulp.task('clean', function(cb) {
   return del([package.dest.dist+'/**', package.dest.dist], cb);
 })
 
+.task('deploy',['clean', 'index', 'less:min', 'js:min'], function() {
+  aws = JSON.parse(fs.readFileSync('./aws.json'));
+  return gulp.src('./dist/**')
+    .pipe(s3(aws));
+})
+
+.task('index', function() {
+  setTimeout(function() {
+    return gulp.src('./index.html')
+      .pipe(gulp.dest(package.dest.dist));
+  },100);
+})
+
 /**
  * Running browser sync server
  */
-.task('server', ['less', 'js'], function(cb) {
+.task('server', ['index', 'less', 'js'], function(cb) {
   return browserSync.init({
     server: {
-      baseDir: './',
+      baseDir: './dist',
       middleware: [ historyApiFallback({
         logger: console.log.bind(console)
       }) ]
@@ -43,7 +58,7 @@ gulp.task('clean', function(cb) {
     browser: "google chrome"
   }, cb);
 })
-.task('server:min', ['less:min', 'js:min'], function(cb) {
+.task('server:min', ['index', 'less:min', 'js:min'], function(cb) {
   return browserSync.init({
     server: {
       baseDir: './'
